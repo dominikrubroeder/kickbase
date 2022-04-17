51bfef74e53e8c13527fae4c02fbc706895a776d
Dominik Rubröder <dominik.rubroeder@icloud.com>
2022-04-17 12:06:22 +0200

    Initial Commit

---
+++ kickbase/kickbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  kickbaseApp.swift
+//  kickbase
+//
+//  Created by Dominik Rubröder on 17.04.22.
+//
+
+import SwiftUI
+
+@main
+struct kickbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
